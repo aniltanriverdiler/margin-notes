e817643ad30c85e3df6fe0d826c5f4756e902eb2
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-03-16 13:03:16 +0300

    chore: initialize Next.js 16 project with shadcn/ui and global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Home = () => {
   return (
-    <h1 className='text-3xl text-red-600'>Welcome Anil !</h1>
-  )
-}
+    <div>
+      <h1 className="text-3xl text-red-600">Welcome Bookified !</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
